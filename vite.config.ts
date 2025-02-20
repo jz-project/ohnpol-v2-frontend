2b958c43f48468ed2d7657ec293c1eb8c51f594a
Hyerim <110741@daum.net>
2025-02-20 22:22:01 +0900

    [Init] 초기 환경 구성

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import react from "@vitejs/plugin-react";
+
+const viteConfig = defineConfig((env) => {
+  const isDevMode = env.mode.includes("development");
+
+  return {
+    base: isDevMode ? "/" : "/ohnpol-v2-frontend/",
+    plugins: [
+      react({
+        jsxRuntime: "automatic",
+      }),
+      tailwindcss(),
+    ],
+    server: {
+      host: "localhost",
+      port: 3000,
+    },
+    preview: {
+      host: "localhost",
+      port: 8080,
+    },
+    css: {
+      devSourcemap: true,
+      modules: {
+        localsConvention: "camelCase",
+        generateScopedName: isDevMode
+          ? "_[local]_[hash:base64:3]"
+          : "_[hash:base64:6]",
+      },
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
+});
+
+export default viteConfig;
