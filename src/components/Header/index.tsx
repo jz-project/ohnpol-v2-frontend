175108fbd9d247d9311b4018a8d03c4d3990536a
gofla1996 <110741@daum.net>
2025-03-27 08:08:33 +0900

    [Modify] 헤더 마크업 구조 변경 - header > nav > ~~~

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,44 +4,46 @@ import { OhnpolLogo } from '../OhnpolLogo';
 
 function Header() {
   return (
-    <nav
+    <header
       className={tm(
         'w-full h-17 py-5 px-7',
         'fixed top-0 bg-neutral-200 z-10',
-        'flex-1 justify-between items-center',
+        'flex justify-center items-center',
         'text-lg font-medium'
       )}
     >
-      <ul
-        className={tm('flex flex-row justify-between items-center', 'h-full')}
-      >
-        <li>
-          <NavLink to="/main">
-            <h2 className="sr-only">메인로고</h2>
-            <OhnpolLogo size={44} />
-          </NavLink>
-        </li>
-        <ul className="flex flex-row gap-7">
-          <ul className="flex flex-row gap-4">
+      <nav className={tm('flex-1 justify-between items-center')}>
+        <ul
+          className={tm('flex flex-row justify-between items-center', 'h-full')}
+        >
+          <li>
+            <NavLink to="/main">
+              <h2 className="sr-only">메인로고</h2>
+              <OhnpolLogo size={44} />
+            </NavLink>
+          </li>
+          <ul className="flex flex-row gap-7">
+            <ul className="flex flex-row gap-4">
+              <li>
+                <NavLink to="/editor">편집기</NavLink>
+              </li>
+              <li>
+                <NavLink to="/community">커뮤니티</NavLink>
+              </li>
+              <li>
+                <NavLink to="/mypage">마이페이지</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">로그아웃</NavLink>
+              </li>
+            </ul>
             <li>
-              <NavLink to="/editor">편집기</NavLink>
-            </li>
-            <li>
-              <NavLink to="/community">커뮤니티</NavLink>
-            </li>
-            <li>
-              <NavLink to="/mypage">마이페이지</NavLink>
-            </li>
-            <li>
-              <NavLink to="/">로그아웃</NavLink>
+              <p>유저프로필</p>
             </li>
           </ul>
-          <li>
-            <p>유저프로필</p>
-          </li>
         </ul>
-      </ul>
-    </nav>
+      </nav>
+    </header>
   );
 }
 export default Header;
