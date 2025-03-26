5356aba9cb8a019e54a5b09e9c8484df122709c8
sooje0ng <tnwjd7890@naver.com>
2025-03-26 21:15:29 +0900

    [Feat] temp data 추가 #11

---
+++ src/temp/community/allPost.ts
@@ -0,0 +1,124 @@
+const allPost = [
+  {
+    postId: 11,
+    polaroid:
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
+    enterComp: 'BigHitMusic',
+    groupName: '방탄소년단',
+    memberName: '지민',
+    albumName: '<BUTTER>',
+    userId: 4,
+    nickname: 'user4',
+    likeQuant: 0,
+  },
+  {
+    postId: 10,
+    polaroid:
+      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/7a18d7b1bcc6235f3a56564a0394f3544c995543e15dd1e0a9910d851b7ccf14',
+    enterComp: 'BigHitMusic',
+    groupName: '방탄소년단',
+    memberName: '진',
+    albumName: '<BUTTER>',
+    userId: 3,
+    nickname: 'user3',
+    likeQuant: 4,
+  },
+  {
+    postId: 9,
+    polaroid:
+      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/9858957520a9106e49dc735ba0637ed03a6bcef5696e098d8b793542c832746c',
+    enterComp: 'BigHitMusic',
+    groupName: '방탄소년단',
+    memberName: '지민',
+    albumName: '<BUTTER>',
+    userId: 3,
+    nickname: 'user3',
+    likeQuant: 1,
+  },
+  {
+    postId: 8,
+    polaroid:
+      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/431954e6ad2df84910f0eedb639526d704e8bbfc6fcabfe82bbfd6cbaa6d97e9',
+    enterComp: 'BigHitMusic',
+    groupName: '방탄소년단',
+    memberName: '뷔',
+    albumName: '<BUTTER>',
+    userId: 2,
+    nickname: 'user2',
+    likeQuant: 3,
+  },
+  {
+    postId: 7,
+    polaroid:
+      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/90b5ba7aaddcf68f45b8ad2b478b2867bf07e8c4cc54ce42539b3ac8c6b28699',
+    enterComp: 'BigHitMusic',
+    groupName: '방탄소년단',
+    memberName: '뷔',
+    albumName: '<BUTTER>',
+    userId: 2,
+    nickname: 'user2',
+    likeQuant: 2,
+  },
+  {
+    postId: 6,
+    polaroid:
+      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/2503cffd5787ce5b5b1b4df37dd47db7a4aa7cb11284221467f96f9fbb73a171',
+    enterComp: 'BigHitMusic',
+    groupName: '방탄소년단',
+    memberName: '뷔',
+    albumName: '<BUTTER>',
+    userId: 1,
+    nickname: 'ohnpol1004',
+    likeQuant: 1,
+  },
+  {
+    postId: 5,
+    polaroid:
+      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/af88baee19b3e1ae483d2a089b187ae73ee4e3fe42dcd04a222d4e5d92f399f7',
+    enterComp: 'BigHitMusic',
+    groupName: '방탄소년단',
+    memberName: '제이홉',
+    albumName: '<BUTTER>',
+    userId: 1,
+    nickname: 'ohnpol1004',
+    likeQuant: 2,
+  },
+  {
+    postId: 4,
+    polaroid:
+      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/8675811599ffddaf4dae0c69752031927dff8bff50c8d469399953058498dd33',
+    enterComp: 'BigHitMusic',
+    groupName: '방탄소년단',
+    memberName: '제이홉',
+    albumName: '<BUTTER>',
+    userId: 1,
+    nickname: 'ohnpol1004',
+    likeQuant: 0,
+  },
+  {
+    postId: 3,
+    polaroid:
+      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/13f53d2579d222211ba328bd9e8eff6ae3e2ca2eaad307f00be2f4f19a0f0acb',
+    enterComp: 'BigHitMusic',
+    groupName: '방탄소년단',
+    memberName: '지민',
+    albumName: '<BUTTER>',
+    userId: 1,
+    nickname: 'ohnpol1004',
+    likeQuant: 0,
+  },
+  {
+    postId: 2,
+    polaroid:
+      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/4ca7757a4ae91ab95d3a7809940137b4e72632b877d3b66f07f6e78ffd0170bd',
+    enterComp: 'BigHitMusic',
+    groupName: '방탄소년단',
+    memberName: '정국',
+    albumName: '<BUTTER>',
+    userId: 1,
+    nickname: 'ohnpol1004',
+    likeQuant: 1,
+  },
+];
+
+export default allPost;
---
+++ src/temp/community/memberProfile.ts
@@ -0,0 +1,39 @@
+const memberProfile = [
+  {
+    name: '정국',
+    memPhoto:
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-jk.jpg?table=block&id=07446464-7b3c-4a75-bc59-1a79d374a501&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
+  },
+  {
+    name: '뷔',
+    memPhoto:
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
+  },
+  {
+    name: '지민',
+    memPhoto:
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-jimin.jpg?table=block&id=5ad6b9f1-0073-4bc2-b55e-9cf780280607&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
+  },
+  {
+    name: '슈가',
+    memPhoto:
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-suga.jpg?table=block&id=96c520fc-11b9-401a-934d-2e5c54cb022e&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
+  },
+  {
+    name: '진',
+    memPhoto:
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-jin.jpg?table=block&id=2ea833ad-b396-4175-bba5-0660db04402c&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
+  },
+  {
+    name: 'RM',
+    memPhoto:
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-rm.jpg?table=block&id=d49f8687-0a4f-4517-9d20-5a0869e238a2&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
+  },
+  {
+    name: '제이홉',
+    memPhoto:
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-jhope.jpg?table=block&id=685a3097-eac4-4cba-b1a7-0910a998503b&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
+  },
+];
+
+export default memberProfile;
