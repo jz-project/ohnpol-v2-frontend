3ef96ac2908f947fd3a38cb9e361938aabc79b13
sooje0ng <tnwjd7890@naver.com>
2025-03-30 18:50:54 +0900

    [Feat] community 페이지 UI 퍼블리싱 완료 #11

--- src/pages/community/page.tsx
+++ src/pages/community/page.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import ArtistProfile from '@/components/ArtistProfile';
-import OhnpolHeader from '@/components/ohnpol-header';
+import Header from '@/components/Header';
 import artistImage from '@/temp/images/방탄소년단.webp';
 import allPost from '@/temp/community/allPost.ts';
 import memberProfile from '@/temp/community/memberProfile.ts';
@@ -10,7 +10,7 @@ function CommunityPage() {
 
   return (
     <>
-      <OhnpolHeader />
+      <Header />
       <ArtistProfile
         artistInfo={{
           photo: artistImage,
@@ -20,6 +20,7 @@ function CommunityPage() {
           collectionQuant: 23,
         }}
       />
+      {/* 멤버별 이미지 */}
       <div className="flex flex-col items-center w-full h-fit pt-5">
         <div className="flex justify-center items-center w-[90%] h-[50vh] gap-x-4">
           {memberProfile?.map((item, index) => (
@@ -27,10 +28,10 @@ function CommunityPage() {
               <p className="text-center font-semibold">{item?.name}</p>
 
               <div
-                className="flex justify-center items-center w-[180px] h-[261px] mx-2 my-2 rounded-[20px] border border-gray-300 bg-gray-300 shadow-lg overflow-hidden cursor-pointer transition-transform duration-100 hover:scale-110"
+                className="w-full max-w-[180px] min-w-[100px] aspect-[3/4.35] mx-2 my-2 rounded-[20px] border border-gray-300 bg-gray-300 shadow-lg overflow-hidden cursor-pointer transition-transform duration-100 hover:scale-110"
                 onClick={() => setClickedMember(item?.name)}
               >
-                <div className="flex justify-center items-center w-[180px] h-[261px]">
+                <div className="flex justify-center items-center w-full h-full">
                   <img
                     src={item?.memPhoto}
                     alt="memberPhoto"
@@ -41,18 +42,18 @@ function CommunityPage() {
             </div>
           ))}
         </div>
-        <div className="w-[95%] bg-[#ede5e5] mt-12 mb-12 rounded-[15px] py-[120px]">
+        <div className="w-[95%] md:w-[85%] bg-[#ede5e5] mt-6 md:mt-20 mb-6 md:mb-12 rounded-[15px] py-[10vh] relative flex flex-col">
           {/* 전체보기 버튼 */}
           <button
-            className="absolute top-[755px] right-[30px] w-[80px] h-[35px] rounded-[18px] border-none mr-[20px] bg-primary-300 text-white float-right text-[16px] cursor-pointer transition-all duration-100 hover:brightness-90"
+            className="absolute top-[-6%] right-[0vw] w-[20vw] max-w-[80px] h-[8vw] max-h-[35px] rounded-[18px] border-none bg-primary-300 text-white text-[5vw] md:text-[16px] cursor-pointer transition-all duration-100 hover:brightness-90"
             onClick={() => {
-              setClickedMember(null);
+              // todo: 함수추가 필요
             }}
           >
             전체보기
           </button>
 
-          {/* 컨텐츠 영역 */}
+          {/* 도안 */}
           <div className="mx-auto flex flex-wrap justify-center items-center w-[85%] h-[90%]">
             {clickedMember === null ? (
               allPost.map((item) => (
--- src/temp/community/allPost.ts
+++ src/temp/community/allPost.ts
@@ -14,7 +14,7 @@ const allPost = [
   {
     postId: 10,
     polaroid:
-      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/7a18d7b1bcc6235f3a56564a0394f3544c995543e15dd1e0a9910d851b7ccf14',
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
     enterComp: 'BigHitMusic',
     groupName: '방탄소년단',
     memberName: '진',
@@ -26,7 +26,7 @@ const allPost = [
   {
     postId: 9,
     polaroid:
-      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/9858957520a9106e49dc735ba0637ed03a6bcef5696e098d8b793542c832746c',
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
     enterComp: 'BigHitMusic',
     groupName: '방탄소년단',
     memberName: '지민',
@@ -38,7 +38,7 @@ const allPost = [
   {
     postId: 8,
     polaroid:
-      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/431954e6ad2df84910f0eedb639526d704e8bbfc6fcabfe82bbfd6cbaa6d97e9',
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
     enterComp: 'BigHitMusic',
     groupName: '방탄소년단',
     memberName: '뷔',
@@ -50,7 +50,7 @@ const allPost = [
   {
     postId: 7,
     polaroid:
-      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/90b5ba7aaddcf68f45b8ad2b478b2867bf07e8c4cc54ce42539b3ac8c6b28699',
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
     enterComp: 'BigHitMusic',
     groupName: '방탄소년단',
     memberName: '뷔',
@@ -62,7 +62,7 @@ const allPost = [
   {
     postId: 6,
     polaroid:
-      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/2503cffd5787ce5b5b1b4df37dd47db7a4aa7cb11284221467f96f9fbb73a171',
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
     enterComp: 'BigHitMusic',
     groupName: '방탄소년단',
     memberName: '뷔',
@@ -74,7 +74,7 @@ const allPost = [
   {
     postId: 5,
     polaroid:
-      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/af88baee19b3e1ae483d2a089b187ae73ee4e3fe42dcd04a222d4e5d92f399f7',
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
     enterComp: 'BigHitMusic',
     groupName: '방탄소년단',
     memberName: '제이홉',
@@ -86,7 +86,7 @@ const allPost = [
   {
     postId: 4,
     polaroid:
-      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/8675811599ffddaf4dae0c69752031927dff8bff50c8d469399953058498dd33',
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
     enterComp: 'BigHitMusic',
     groupName: '방탄소년단',
     memberName: '제이홉',
@@ -98,7 +98,7 @@ const allPost = [
   {
     postId: 3,
     polaroid:
-      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/13f53d2579d222211ba328bd9e8eff6ae3e2ca2eaad307f00be2f4f19a0f0acb',
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
     enterComp: 'BigHitMusic',
     groupName: '방탄소년단',
     memberName: '지민',
@@ -110,7 +110,7 @@ const allPost = [
   {
     postId: 2,
     polaroid:
-      'https://ohnpol.s3.ap-northeast-2.amazonaws.com/polaroid/4ca7757a4ae91ab95d3a7809940137b4e72632b877d3b66f07f6e78ffd0170bd',
+      'https://jeweled-bellflower-76d.notion.site/image/https%3A%2F%2Fibighit.com%2Fbts%2Fimages%2Fprofile%2Fproof%2Fmember%2Fmember-v.jpg?table=block&id=3f8c7667-3932-4748-9f34-e6760e9004ef&spaceId=54d3ee14-4578-4132-99d0-a6502923be62&width=2000&userId=&cache=v2',
     enterComp: 'BigHitMusic',
     groupName: '방탄소년단',
     memberName: '정국',
