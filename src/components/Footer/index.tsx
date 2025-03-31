4e86b1890aae7715763570ae669362c35876f382
gofla1996 <110741@daum.net>
2025-03-31 22:38:22 +0900

    [Design] 푸터 CSS 수정
    - 배경색상 제거
    - 글씨 색상 변경

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,9 +4,8 @@ function Footer() {
   return (
     <footer
       className={tm(
-        'w-full text-neutral-100 mt-17 py-4 text-center',
-        'text-sm',
-        'bg-neutral-800'
+        'w-full text-neutral-800 mt-17 py-4 text-center',
+        'text-sm'
       )}
     >
       <p>&copy; 2025 - All rights reserved</p>
