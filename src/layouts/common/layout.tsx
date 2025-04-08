fa4c0b8513c2e3cbd11c4b9c92e855e2b0cd972b
gofla1996 <110741@daum.net>
2025-04-08 21:42:22 +0900

    [Refactor] 레이아웃 불필요한 리렌더링 개선
    - 헤더 푸터 useMemo 처리

--- src/layouts/common/layout.tsx
+++ src/layouts/common/layout.tsx
@@ -1,15 +1,19 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Outlet } from 'react-router';
+import { useMemo } from 'react';
 
 function Layout() {
+  const header = useMemo(() => <Header />, []);
+  const footer = useMemo(() => <Footer />, []);
+
   return (
     <div className="w-full min-h-screen flex flex-col items-center">
-      <Header />
+      {header}
       <main className="w-full mt-17 flex flex-1 flex-col items-center">
         <Outlet />
       </main>
-      <Footer />
+      {footer}
     </div>
   );
 }
